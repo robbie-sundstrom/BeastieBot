6972f0161cb1a192f417036b2781408f72215429
Frackleton <mack.frackleton@gmail.com>
2015-04-04 15:38:00 -0400

    tweaked

--- betterwikiAPI.py
+++ betterwikiAPI.py
@@ -1,12 +1,18 @@
 #must first sudo pip install wikipedia 
 #must also be run from terminal, don't cntrl+B in a text editor
 import wikipedia 
+import ast
+
 def content(title):
 	#make sure the article exists first
 	if wikipedia.search(title):
-
 		page = wikipedia.page(title)
-		return page.content
+		content=(page.content)
+		content_list =content.split(' ')
+		for word in content_list:
+			if word =='American': # replace with an expression that generalizes for words rhyming with user input
+				return word
+
 	else:
 		raise ValueError 
 
