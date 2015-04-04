e2ab50f0acb49ac6f98f8b43450f0df0da85b832
Frackleton <mack.frackleton@gmail.com>
2015-04-04 14:13:04 -0400

    updated to test for existing articles

--- betterwikiAPI.py
+++ betterwikiAPI.py
@@ -1,4 +1,14 @@
 #must first sudo pip install wikipedia 
 #must also be run from terminal, don't cntrl+B in a text editor
 import wikipedia 
-print wikipedia.summary("Beastie Boys")
+def content(title):
+	#make sure the article exists first
+	if wikipedia.search(title):
+
+		page = wikipedia.page(title)
+		return page.content
+	else:
+		raise ValueError 
+
+if __name__ == '__main__':
+	print content('Beastie Boys')
