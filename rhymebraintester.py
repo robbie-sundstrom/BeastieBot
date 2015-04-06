0c47844f9910689ae14bb80c85952e59d32fd133
Bill Wong <billmwong@gmail.com>
2015-04-05 23:27:45 -0400

    Sorts by frequency, uses JSON now

--- rhymebraintester.py
+++ rhymebraintester.py
@@ -1,29 +1,25 @@
 """
 Generates a list of rhymes sorted by popularity and rhyme distance.
+
 """
 
-import urllib2, json
+import urllib2, json, pprint
 
 def find_rhymes(sourceword):
 	url = "http://rhymebrain.com/talk?function=getRhymes&word=" + sourceword
 	f = urllib2.urlopen(url)
 	rawrhymes = f.read()
 
-	# split the text into a list of lines, ignore leading '['
-	# NOTE: does not take care of last ']'
-	lines = rawrhymes[1:].splitlines()
-	for i in range(len(lines)):
-		lines[i] = lines[i][2:-2]	# get rid of brackets on either side
+	rhymes_dicts_list = json.loads(rawrhymes)
+
+	perfect_rhymes_dicts_list = []
+
+	for diction in rhymes_dicts_list:
+		if diction['score'] >= 300:
+			perfect_rhymes_dicts_list.append(diction)
+	sorted_list = sorted(perfect_rhymes_dicts_list, key=lambda k: k['freq'], reverse=True)
 
 	wordlist = []
-	for line in lines:
-		startchar = 8	# start at the actual word
-		for i in range(8, len(line[7:])):
-			if line[i] == '"':	# search for the closing quote
-				stopchar = i
-				break
-		wordlist.append(line[startchar:stopchar] + '.')
-
+	for diction in sorted_list:
+		wordlist.append(str(diction['word']))
 	return wordlist
-
-# print find_rhymes('tack')
