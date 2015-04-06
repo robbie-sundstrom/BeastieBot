0e568b7450f2ad1452e08041f6f43d489f926e57
rsiegel812 <robert.siegel@students.olin.edu>
2015-04-05 21:24:54 -0400

    Changed Output to String

--- TwitterSearchDemo.py
+++ TwitterSearchDemo.py
@@ -11,7 +11,11 @@ def find_tweets(sourceword,limit):
 
     # create TwitterSearch object
     ts = TwitterSearch(
-        ### PASTE KEYS HERE ###
+        consumer_key = 'IIS9bxYviganFNmhRI16FpOAW',
+        consumer_secret = 'Av9guGhtBTSMyYXAZZgYzSQMNSDgBtHfG3aYrO8L4Uzix7704a',
+        access_token = '218702250-J5CiB7cfx8jhIKIB2wQqR1ZBV6o0yPN57x5cF4Gp',
+        access_token_secret = '23SnU89U18ftyoMJTaJYgEKc5jMCJNfDq1WKXDiz1gAYG'
+
     )
 
     counter = 0
@@ -23,7 +27,7 @@ def find_tweets(sourceword,limit):
 
         thistweet = tweet['text']
         if '@' not in thistweet and thistweet[-(len(sourceword)):] == sourceword:
-            tweetlist.append(thistweet)
+            tweetlist.append(str(thistweet))
             counter += 1
     return tweetlist
 
