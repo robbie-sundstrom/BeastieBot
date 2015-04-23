fc56d9bee56b932c008d9bf0e29ecf8b593e82e2
rsiegel812 <robert.siegel@students.olin.edu>
2015-04-22 21:20:01 -0400

    Updated GUI

--- gui.py
+++ gui.py
@@ -1,10 +1,15 @@
 from Tkinter import *
 from BeastieAudio import *
+from BeastieBot2 import *
+from subproc_combo import *
 
 class BeastieInterface:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
+        
+        self.word = StringVar() #Input Word
+        self.lines = StringVar() #Input Number of Lines
         
         #Quit Button
         self.button = Button(frame, 
@@ -12,11 +17,11 @@ class BeastieInterface:
                          command=frame.quit)
         self.button.grid(row=0,column=0,sticky=W)
         
-        #Welcome Button
-        self.slogan = Button(frame,
-                         text="Welcome",
-                         command=self.write_slogan)
-        self.slogan.grid(row=0,column=1)
+        # #Welcome Button
+        # self.slogan = Button(frame,
+        #                  text="Welcome",
+        #                  command=self.write_slogan)
+        # self.slogan.grid(row=0,column=1)
         
         #Record Button
         self.audio = Button(frame,
@@ -28,34 +33,52 @@ class BeastieInterface:
         self.play = Button(frame,
                          text="Play Rap",
                          command=self.play)
-        self.play.grid(row=1,column=2)
+        self.play.grid(row=0,column=1, sticky=E)
 
-        self.return_theme = Button(frame,
-                         text="Enter",
-                         command=self.fetch)
-        self.return_theme.grid(row=2, column=2)        
-        
-    
-        self.v = StringVar()
+        #Text Entry and Label
         self.entry_label = Label(frame,
         	             text="Inspirative Word:")
         self.entry_label.grid(row=2,column=0)
 
         self.entry = Entry(frame,
-                         textvariable=self.v)
+                         textvariable=self.word)
         self.entry.grid(row=2,column=1)
-    
+
+        #Number Entry and Label
+        self.entry_label = Label(frame,
+        	             text="Number of Lines:")
+        self.entry_label.grid(row=3,column=0)
+
+        self.entry = Entry(frame,
+                         textvariable=self.lines)
+        self.entry.grid(row=3,column=1)
+
+        #Enter Button 
+        self.return_theme = Button(frame,
+                         text="Enter",
+                         command=self.fetch)
+        self.return_theme.grid(row=4, column=1, sticky=E)        
+        
+
+
     def write_slogan(self):
         print "Welcome to Beastie Bot! Enter an inspirative word and get ready to spit some hot fire."
 
     def record(self):
-        arecord()
+        combo_record()
 
     def play(self):
     	aplay()
         
     def fetch(self):
-        print self.v.get()
+        rap = beastie_it_up(self.word.get(), int(self.lines.get()))
+         
+
+# class Rap(lines):
+#     lines = lines
+#     beastie_it_up(self.word.get(), int(self.lines.get()))
+#     for line in rap:
+#         print line 
 
 root = Tk()
 app = BeastieInterface(root)
