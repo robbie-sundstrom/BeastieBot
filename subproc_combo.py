dd445966be3c9265c5fac94cab701df8fac8205a
Bill Wong <billmwong@gmail.com>
2015-05-05 18:18:19 -0400

    Cleaned up subproc_combo.py

--- subproc_combo.py
+++ subproc_combo.py
@@ -10,23 +10,11 @@ def record():
 	subprocess.call(a1, shell=True)
 
 def combo1():
-	# while time.time() <= start_time:
-		# pass
 	threading.Thread(target=record).start()
 
 def combo2():
-	# while time.time()<=start_time:
-		# pass
 	threading.Thread(target=play).start()
-# start_time =time.time()+20
 
 def combostart():
-	# start_time = time.time()+10
-	# while time.time() < start_time:
-	# 	pass
 	threading.Thread(target=combo1).start()
 	threading.Thread(target=combo2).start()
-
-
-#if __name__ == '__main__':
-#	combo()
